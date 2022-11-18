fb6ac2e97221740d676c4e19d990c87b405372fb
Neil Cabug-os <neilcabug-os@Neils-MacBook-Pro.local>
2022-11-18 08:03:48 +0800

    :tada: Added dropdown selection

---
+++ lib/src/models/selection_model.dart
@@ -0,0 +1,16 @@
+class SelectionModel {
+  String? code;
+  String? name;
+  String? regionCode;
+  dynamic provinceCode;
+
+  SelectionModel({this.code, this.name, this.regionCode, this.provinceCode});
+
+  factory SelectionModel.fromJson(Map<String, dynamic> json) => SelectionModel(
+        code: json.containsKey('code') ? json['code'] : '',
+        name: json.containsKey('name') ? json['name'] : '',
+        regionCode: json.containsKey('regionCode') ? json['regionCode'] : '',
+        provinceCode:
+            json.containsKey('provinceCode') ? json['provinceCode'] : '',
+      );
+}
